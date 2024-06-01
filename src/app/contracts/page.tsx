a40551cfdf0963234b62a82fe80127e6ff0e7d06
robin croft <rob@robincroft.com>
2024-06-01 12:46:15 +0100

    add contracts

--- src/app/contracts/page.tsx
+++ src/app/contracts/page.tsx
@@ -1,9 +1,40 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Contracts() {
+
+  const [contracts, setContracts] = useState([]);
+  const [contractCount, setContractCount] = useState(1000);
+
+  const getContracts = async () => {
+    // @ts-ignore
+    setContracts(undefined)
+    const result = await fetch(`https://mps-api-production-8da5.up.railway.app/contracts?awardedCount=${contractCount}`);
+    const contractsResult = await result.json();
+    setContracts(contractsResult);
+  }
+
+  useEffect(() => {
+    // getContracts();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <h1>contracts</h1>
+    <main className="flex flex-col p-8">
+      <h1>contracts</h1>
+      <div className="ring flex gap-4">
+        <div className="flex flex-col">          
+          <input value={contractCount} onChange={(e) => setContractCount(Number(e.target.value))}></input>
+          <label>Countract count</label>
+        </div>
+        <button onClick={getContracts} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">go</button>        
+      </div>
+      <div> 
+      
+      {!contracts && <progress value="" />}
+      {contracts && <pre>{JSON.stringify(contracts, null, 2)}</pre>}
+      
+      </div>
+      
     </main>
   );
 }
