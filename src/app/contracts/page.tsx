dd7f83df86b33244d92032889668bd4de3189cd7
robin croft <rob@robincroft.com>
2024-06-01 17:45:18 +0100

    make table dynamic

--- src/app/contracts/page.tsx
+++ src/app/contracts/page.tsx
@@ -16,13 +16,39 @@ export default function Contracts() {
 
   const [contracts, setContracts] = useState([]);
   const [contractCount, setContractCount] = useState(1000);
+  const [awardedToName, setAwardedToName] = useState("");
 
-  const getContracts = async () => {
+  const getContractsByCount = async () => {
     // @ts-ignore
     setContracts(undefined)
     const result = await fetch(`https://mps-api-production-8da5.up.railway.app/contracts?awardedCount=${contractCount}`);
     const contractsResult = await result.json();
     setContracts(contractsResult);
+  }
+
+  const getContractsByName = async () => {
+    // @ts-ignore
+    setContracts(undefined)
+    const result = await fetch(`https://mps-api-production-8da5.up.railway.app/contracts?orgName=${awardedToName}`);
+    const contractsResult = await result.json();
+    setContracts(contractsResult);
+  }
+
+  const renderCell = (value) => {
+    let renderdValue = value;
+
+    if (value.low) {
+      renderdValue = value.low;
+    } else if (value.year) {
+      const jsDate = new Date(
+        value.year.low,
+        value.month.low - 1, // Months are 0-indexed in JS (0 = January)
+        value.day.low
+      );
+      renderdValue = jsDate.toLocaleDateString();
+    }
+
+    return renderdValue;
   }
 
   useEffect(() => {
@@ -37,9 +63,20 @@ export default function Contracts() {
           <input value={contractCount} onChange={(e) => setContractCount(Number(e.target.value))}></input>
           <label>Countract count</label>
         </div>
-        <button onClick={getContracts} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">go</button>
+        <button onClick={getContractsByCount} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Contract by Count</button>
       </div>
+
+      <div className="ring flex gap-4">
+        <div className="flex flex-col">
+          <input value={awardedToName} onChange={(e) => setAwardedToName(e.target.value)}></input>
+          <label>Countract count</label>
+        </div>
+        <button onClick={getContractsByName} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Awarded to Name</button>
+      </div>
+
       <div>
+
+      {contracts && !contracts.map && <h1>No data</h1>}
 
         {!contracts && <progress value="" />}
 
@@ -53,10 +90,13 @@ export default function Contracts() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {contracts?.map((contract: any, index) => (
-              <TableRow key={`row-${index}`}>
-                <TableCell className="font-medium">{contract._fields[contract._fieldLookup["org.Name"]]}</TableCell>
-                <TableCell className="font-medium">{contract._fields[contract._fieldLookup["contractCount"]].low}</TableCell>
+            {contracts && Array.isArray(contracts) && contracts.map((contract: any, rowIndex) => (
+              <TableRow key={`row-${rowIndex}`}>
+
+                {contracts && contracts[0]?.keys.map((header, cellIndex) => (
+                  <TableCell key={`cell-${cellIndex}`} className="font-medium">{renderCell(contract._fields[contract._fieldLookup[contract.keys[cellIndex]]])}</TableCell>  
+                ))}
+
               </TableRow>
             ))}
           </TableBody>
