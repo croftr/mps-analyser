075c8958bae848d506d90671b2d74191248cf4b8
robin croft <rob@robincroft.com>
2024-06-02 13:31:14 +0100

    tidy

--- src/app/mp/page.tsx
+++ src/app/mp/page.tsx
@@ -19,10 +19,8 @@ export default function Mp() {
   const male = <svg className="standalone-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24"><path d="M16 2v2h3.586l-3.972 3.972c-1.54-1.231-3.489-1.972-5.614-1.972-4.97 0-9 4.03-9 9s4.03 9 9 9 9-4.03 9-9c0-2.125-.741-4.074-1.972-5.614l3.972-3.972v3.586h2v-7h-7zm-6 20c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7z" /></svg>
   const female = <svg className="standalone-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24"><path d="M21 9c0-4.97-4.03-9-9-9s-9 4.03-9 9c0 4.632 3.501 8.443 8 8.941v2.059h-3v2h3v2h2v-2h3v-2h-3v-2.059c4.499-.498 8-4.309 8-8.941zm-16 0c0-3.86 3.14-7 7-7s7 3.14 7 7-3.14 7-7 7-7-3.14-7-7z" /></svg>
 
-
-  // const mpDetails = useMpStore(state => state.mp);
-
   const [mpDetails, setMpDetails] = useState<Record<string, any> | undefined>({});
+
   const [votefilterFrom, setVotefilterFrom] = useState("");
   const [votefilterTo, setVotefilterTo] = useState("");
   const [votefilterType, setVotefilterType] = useState("");
@@ -166,7 +164,7 @@ export default function Mp() {
 
 
   return (
-    <section id="mpDetailsPage" className="flex justify-between p-4 max-w-fit">
+    <section id="mpDetailsPage" className="flex justify-around p-4">
 
       <div className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:text-white">
 
