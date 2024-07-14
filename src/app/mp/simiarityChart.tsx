51a346a6445387808dc14f15b5588e4e04c1913e
robin croft <rob@robincroft.com>
2024-07-14 12:23:47 +0100

    party colours

--- src/app/mp/simiarityChart.tsx
+++ src/app/mp/simiarityChart.tsx
@@ -1,11 +1,8 @@
 "use client"
 
 import { Bar, BarChart, XAxis, YAxis, Cell, Label, LabelList } from "recharts"
-import { useState, useEffect } from "react";
 
 import { PARTY_COLOUR } from ".././../app/config/constants";
-// import { PARTY_COLOUR } from "@config/constants";
-
 
 import {
   ChartConfig,
@@ -30,56 +27,37 @@ interface SimilarityChartProps {
 
 export default function SimilarityChart({ mpData, comparedMpName, type, onQueryMpByName }: SimilarityChartProps) {
 
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  const chartConfig: ChartConfig = mpData?.reduce(
-    (config, entry) => {
-      config[entry.name] = {
-        label: entry.name,
-        // Set the fill color to your primary color CSS variable
-        color: PARTY_COLOUR[entry.party]?.backgroundColour || "var(--clr-primary)", 
-      };
-      return config;
-    },
-    {} as ChartConfig
-  ) || {};
-
-
   return (
-      <div>
 
-        {mpData && (
-          <ChartContainer config={chartConfig}>
-            <BarChart
-              data={mpData}              
-              layout="vertical"
-              margin={{ left: 0 }}
-            >
-              <YAxis
-                dataKey="name"
-                type="category"
-                tickLine={false}
-                tickMargin={10}
-                axisLine={false}
-              />
-              <XAxis dataKey="score" type="number" hide />
-              <ChartTooltip
-                cursor={false}
-                content={<ChartTooltipContent hideLabel />}
-              />
-              <Bar dataKey="score" layout="vertical" radius={5} onClick={onQueryMpByName} cursor="pointer">
-                {mpData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={PARTY_COLOUR[entry.party]?.backgroundColour || "var(--clr-primary)"}                     
-                 / >
-                 
-                ))}
-              </Bar>
-            </BarChart>
-          </ChartContainer>
-        )}
-      </div>
+    <ChartContainer config={{}}>
+      <BarChart
+        data={mpData}
+        layout="vertical"
+        margin={{ left: 0 }}
+      >
+        <YAxis
+          dataKey="name"
+          type="category"
+          tickLine={false}
+          tickMargin={10}
+          axisLine={false}
+        />
+        <XAxis dataKey="score" type="number" hide />
+        <ChartTooltip
+          cursor={false}
+          content={<ChartTooltipContent hideLabel />}
+        />
+        <Bar dataKey="score" layout="vertical" radius={5} onClick={onQueryMpByName} cursor="pointer">
+          {mpData.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={PARTY_COLOUR[entry.party]?.backgroundColour || "var(--clr-primary)"}
+            />
+
+          ))}
+        </Bar>
+      </BarChart>
+    </ChartContainer>
 
   )
 }
