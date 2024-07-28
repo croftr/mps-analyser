772bc73619738bf326ec6d0f53258a1309c43a94
robin croft <rob@robincroft.com>
2024-07-28 13:57:40 +0100

    start of contract and org details

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -4,20 +4,40 @@ import { useState, useEffect, useMemo, Suspense } from 'react';
 import ky from 'ky';
 import { config } from '../app.config';
 
-import TradeUnionIcon from './TradeUnionIcon'; 
-import IndividualIcon from './IndividualIcon'; 
-import CompanyIcon from './CompanyIcon'; 
+import TradeUnionIcon from './TradeUnionIcon';
+import IndividualIcon from './IndividualIcon';
+import CompanyIcon from './CompanyIcon';
 
 import { useSearchParams, usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 
 import { DataTable } from "@/components/ui/data-table";
+import NeoTableSkeleton from '@/components/ui/neoTableSkeleton';
 
-const donationSourceTypes = {
-  "Trade Union" : <TradeUnionIcon />,
-  "Individual" : <IndividualIcon />,
-  "Company" : <CompanyIcon />   
+const TYPES = {
+  ALL_PARTIES: "ALL_PARTIES",
+  PARTY: "PARTY",
+  DONAR: "DONAR"
+};
+
+
+// const donationSourceTypes = {
+//   "Trade Union" : <TradeUnionIcon />,
+//   "Individual" : <IndividualIcon />,
+//   "Company" : <CompanyIcon />   
+// }
+
+enum DonationSourceType {
+  TradeUnion = "Trade Union",
+  Individual = "Individual",
+  Company = "Company",
 }
+
+const donationSourceTypes: { [key in DonationSourceType]: JSX.Element } = {
+  [DonationSourceType.TradeUnion]: <TradeUnionIcon />,
+  [DonationSourceType.Individual]: <IndividualIcon />,
+  [DonationSourceType.Company]: <CompanyIcon />,
+};
 
 const donationColumns = [
   {
@@ -61,7 +81,7 @@ const donarDetailsColumns = [
   {
     accessorKey: 'receivedDate',
     header: 'Date',
-  },  
+  },
   {
     accessorKey: 'amount',
     header: 'Amount',
@@ -87,16 +107,16 @@ function PageContent() {
   const [tableColumns, setTableColumns] = useState(donationColumns);
   const [isLoading, setIsLoading] = useState(true);
   const [tableText, setTableText] = useState("");
-  
-  const [donarStatus, setDonarStatus] = useState("")
+  const [type, setType] = useState(TYPES.DONAR)
 
-  const [name, setName] = useState<string|null>("")
+  const [donarStatus, setDonarStatus] = useState<DonationSourceType>(DonationSourceType.Company)
 
   const getData = async () => {
-    
+
+
     const nameParam = searchParams.get('name');
 
-    const donationsResponse:Array<any> = await ky(`${config.mpsApiUrl}donations?donar=${nameParam}`).json();
+    const donationsResponse: Array<any> = await ky(`${config.mpsApiUrl}donations?donar=${nameParam}`).json();
 
     const headerInfo = Array.isArray(donationsResponse) ? donationsResponse[0] : undefined;
 
@@ -109,13 +129,14 @@ function PageContent() {
     setTableData(donationsResponse);
 
     console.log("response ", donationsResponse);
-    
 
-    setName(nameParam);
-    
+
+    setIsLoading(false);
+
   }
 
   useEffect(() => {
+    setIsLoading(true);
     getData();
   }, [router, searchParams]);
 
@@ -123,13 +144,22 @@ function PageContent() {
   return (
 
     <div className="flex flex-col justify-center ring p-4">
-      <h1>{name}</h1>
 
-      <DataTable
+      <div className="flex flex-col md:flex-row md:justify-between p-4">
+
+        <span className='flex gap-2'> {type === TYPES.DONAR ? donationSourceTypes[donarStatus] ? donationSourceTypes[donarStatus] : (donarStatus) : type} <h2 className="text-xl font-semibold text-gray-900 dark:text-white">{tableText}</h2></span>
+        <h3 className="text-sm font-medium text-gray-600 dark:text-gray-400">Total donations since 01-Jan-2000</h3>
+      </div>
+
+      {isLoading ? (
+        <NeoTableSkeleton columns={4} />
+      ) : (
+        <DataTable
           data={tableData}
           columns={tableColumns}
-          onRowClick={() => {}}          
+          onRowClick={() => { }}
         />
+      )}
 
     </div>
 
