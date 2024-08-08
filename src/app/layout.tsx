8b828cb3b6ddbfc9d68472f363db4d344f423dd1
robin croft <rob@robincroft.com>
2024-08-08 17:33:55 +0100

    fix scroll on small screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-background text-foreground"> 
+      <body className="bg-background text-foreground">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Nav />
           {/* <Search /> */}
-          <main className="min-h-[calc(100vh-30px)] overflow-y-auto pb-4 pt-4 mt-0 lg:mt-12 pb-20 sm:pb-12">
+          <main className="min-h-[calc(100vh-30px)] overflow-y-auto pt-4 mt-0 lg:mt-12 pb-20 sm:pb-12"
+            style={{ paddingBottom: "env(safe-area-inset-bottom)" }}
+          >
             {children}
           </main>
         </ThemeProvider>
