9f1e5a7a38ebfa4c980c8e6eb85b95b8a7edb9e2
robin croft <rob@robincroft.com>
2024-08-08 17:27:05 +0100

    fix scroll on small screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,21 @@
-// @ts-nocheck
+// "use client" // If you have Client Components
 import "./globals.css";
-
 import Nav from "@/components/ui/nav";
-import Search from "@/components/ui/search";
-
-import { ThemeProvider } from "@/components/theme-provider"
+// import Search from "@/components/ui/search"; 
+import { ThemeProvider } from "@/components/theme-provider";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-
+}) {
   return (
     <html lang="en">
-      
-      <body className="bg-background text-foreground overflow-hidden">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        > 
+      <body className="bg-background text-foreground"> 
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Nav />
           {/* <Search /> */}
-          <main className="h-[calc(100vh-34px)] overflow-y-scroll pb-4 pt-4 mt-0 lg:mt-12">
+          <main className="min-h-[calc(100vh-30px)] overflow-y-auto pb-4 pt-4 mt-0 lg:mt-12 pb-20 sm:pb-12">
             {children}
           </main>
         </ThemeProvider>
