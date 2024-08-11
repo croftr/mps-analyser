d3978cdc9df975662e94799c2fa9616d0fe36901
robin croft <rob@robincroft.com>
2024-08-11 14:04:45 +0100

    org details collaspse components and spinners

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useMemo, Suspense } from 'react';
+import { useState, useEffect, Suspense } from 'react';
 import ky from 'ky';
 import { config } from '../app.config';
 
@@ -8,26 +8,28 @@ import TradeUnionIcon from './TradeUnionIcon';
 import IndividualIcon from './IndividualIcon';
 import CompanyIcon from './CompanyIcon';
 
-import { useSearchParams, usePathname } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 
 import { DataTable } from "@/components/ui/data-table";
 import NeoTableSkeleton from '@/components/ui/neoTableSkeleton';
 import { NeoTable } from '@/components/ui/neoTable';
-import { Switch } from "@/components/ui/switch"
+
+import { Button } from "@/components/ui/button"
+
+import { ArrowUp } from "lucide-react"
+import { ArrowDown } from "lucide-react"
+
+import {
+  Collapsible,
+  CollapsibleContent
+} from "@/components/ui/collapsible"
 
 const TYPES = {
   ALL_PARTIES: "ALL_PARTIES",
   PARTY: "PARTY",
   DONAR: "DONAR"
 };
-
-
-// const donationSourceTypes = {
-//   "Trade Union" : <TradeUnionIcon />,
-//   "Individual" : <IndividualIcon />,
-//   "Company" : <CompanyIcon />   
-// }
 
 enum DonationSourceType {
   TradeUnion = "Trade Union",
@@ -60,20 +62,6 @@ const donationColumns = [
   },
 ];
 
-const partyDonarColumns = [
-  {
-    accessorKey: 'donar',
-    header: 'Donar',
-  },
-  {
-    accessorKey: 'donatedCout',
-    header: 'Donation Count',
-  },
-  {
-    accessorKey: 'totalDonationValue',
-    header: 'Total Value',
-  }
-]
 
 const donarDetailsColumns = [
   {
@@ -107,21 +95,19 @@ function PageContent() {
 
   const router = useRouter();
   const searchParams = useSearchParams();
-  const pathname = usePathname();
+
+  const [isContractsDown, setIsContractsDown] = useState(false);
+  const [isDonationssDown, setIsDonationssDown] = useState(false);
 
   const [tableData, setTableData] = useState<Array<any>>([]);
   const [tableColumns, setTableColumns] = useState(donationColumns);
   const [isLoading, setIsLoading] = useState(true);
-
-  const [showContracts, setShowContracts] = useState(false);
-  const [showDonations, setShowDonations] = useState(false);
 
   const [tableText, setTableText] = useState("");
 
   const [type, setType] = useState(TYPES.DONAR)
   const [name, setName] = useState<string | null>("")
   const [contracts, setContracts] = useState<Array<any> | undefined>()
-
 
   const [donarStatus, setDonarStatus] = useState<DonationSourceType>(DonationSourceType.Company)
 
@@ -149,8 +135,8 @@ function PageContent() {
     setIsLoading(false);
   }
 
-  const showContract = (row:any) => {
-    console.log(row);    
+  const showContract = (row: any) => {
+    console.log(row);
     router.push(`contract?supplier=${row._fields[1]}&title=${row._fields[0]}&value=${row._fields[3]}&awardedby=${row._fields[2]}`, { scroll: true });
   }
 
@@ -159,6 +145,13 @@ function PageContent() {
     getData();
   }, [router, searchParams]);
 
+  const onToggleContracts = () => {
+    setIsContractsDown(!isContractsDown);
+  }
+
+  const onToggleDonations = () => {
+    setIsDonationssDown(!isDonationssDown);
+  }
 
   return (
 
@@ -170,44 +163,61 @@ function PageContent() {
         <h4 className="font-semibold text-lg mb-2">{name}</h4>
       </span>
 
+      <div className="flex">
+        <Button
+          variant='outline'
+          onClick={onToggleContracts}
+          className='flex gap-2'
+        >
+          {isLoading ? <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-gray-400"></div> : contracts ? contracts.length : 0} Contracts Received
+          {isContractsDown ? <ArrowUp /> : <ArrowDown />}
+        </Button>
 
-      <div className="flex gap-4 mb-4">
-        <div className='flex gap-2'>
-          <p>{contracts ? contracts.length : 0} Contracts Received</p>
-          <Switch id="vaexclude" checked={showContracts} onCheckedChange={() => setShowContracts(!showContracts)} />
-        </div>
-
-        <div className='flex gap-2'>
-          <p>{tableData ? tableData.length : 0} Donations Made</p>
-          <Switch id="vaexclude" checked={showDonations} onCheckedChange={() => setShowDonations(!showDonations)} />
-        </div>
-
+        <Button
+          variant='outline'
+          onClick={onToggleDonations}
+          className='flex gap-2'
+        >
+          {isLoading ? <div className="animate-spin rounded-full h-5 w-5 border-t-2 border-b-2 border-gray-400"></div> : tableData ? tableData.length : 0} Donations Made
+          {isDonationssDown ? <ArrowUp /> : <ArrowDown />}
+        </Button>
       </div>
 
+      <Collapsible
+        open={isContractsDown}        
+        className="flex w-full"
+      >
 
-      {showContracts && <NeoTable data={contracts} title="Contracts Received" onRowClick={showContract} />}
+        <CollapsibleContent className="flex flex-col w-full gap-2">
+          <NeoTable data={contracts} title="Contracts Received" onRowClick={showContract} />
+        </CollapsibleContent>
+      </Collapsible>
 
-      {showDonations && (<div>
-        <div className="flex flex-col md:flex-row md:justify-between p-4">
-          <h2 className="text-xl font-semibold text-gray-900 dark:text-white">{tableText}</h2>
-          <h3 className="text-sm font-medium text-gray-600 dark:text-gray-400">Total donations since 01-Jan-2000</h3>
-        </div>
+      <Collapsible
+        open={isDonationssDown}        
+        className="flex w-full"
+      >
 
-        {isLoading ? (
-          <NeoTableSkeleton columns={4} />
-        ) : (
-          <DataTable
-            data={tableData}
-            columns={tableColumns}
-            onRowClick={() => { }}
-          />
-        )}
-      </div>)}
+        <CollapsibleContent className="flex flex-col w-full gap-2">
+          <div>
+            <div className="flex flex-col md:flex-row md:justify-between p-4">
+              <h2 className="text-xl font-semibold text-gray-900 dark:text-white">{tableText}</h2>
+              <h3 className="text-sm font-medium text-gray-600 dark:text-gray-400">Total donations since 01-Jan-2000</h3>
+            </div>
 
-
-
+            {isLoading ? (
+              <NeoTableSkeleton columns={4} />
+            ) : (
+              <DataTable
+                data={tableData}
+                columns={tableColumns}
+                onRowClick={() => { }}
+              />
+            )}
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
     </div>
-
   );
 
 }
