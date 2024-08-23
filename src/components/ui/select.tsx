4644c3195ca48c0669fab30e4b6ebfda5e14ad31
robin croft <rob@robincroft.com>
2024-08-23 19:05:11 +0100

    categories array

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -39,7 +39,7 @@ const SelectScrollUpButton = React.forwardRef<
   <SelectPrimitive.ScrollUpButton
     ref={ref}
     className={cn(
-      "flex cursor-default items-center justify-center py-1",
+      "flex cursor-default items-center justify-center py-4 border-b-2",
       className
     )}
     {...props}
@@ -56,7 +56,7 @@ const SelectScrollDownButton = React.forwardRef<
   <SelectPrimitive.ScrollDownButton
     ref={ref}
     className={cn(
-      "flex cursor-default items-center justify-center py-1",
+      "flex cursor-default items-center justify-center py-4 border-t-2",
       className
     )}
     {...props}
