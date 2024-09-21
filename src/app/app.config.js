49d5a797b3854592e29151c7babbdad0b120c4c9
robin croft <rob@robincroft.com>
2024-09-21 16:59:12 +0100

    use new deployed service!

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,7 +1,8 @@
 export const config = {
-    mpsApiUrl: "https://mps-api-production-8da5.up.railway.app/"
+    // mpsApiUrl: "https://mps-api-production-8da5.up.railway.app/"
     // mpsApiUrl: 'http://localhost:8080/'        
     // mpsApiUrl: 'https://api.commons-connect.com/'        
+    mpsApiUrl: 'https://service.commons-connect.com/'        
  
 }
 
