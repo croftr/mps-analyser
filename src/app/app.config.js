8fe3048798c61a6220245ea9b8bacedfb4fb579c
robin croft <rob@robincroft.com>
2024-10-12 13:16:26 +0100

    change api to use api gateway

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,5 +1,6 @@
 export const config = {
     // mpsApiUrl: 'http://localhost:8080/'           
-    mpsApiUrl: 'https://service.commons-connect.com/'         
+    // mpsApiUrl: 'https://service.commons-connect.com/'         
+    mpsApiUrl: 'https://fk1nq6pkc3.execute-api.eu-north-1.amazonaws.com/dev/'         
 }
 
